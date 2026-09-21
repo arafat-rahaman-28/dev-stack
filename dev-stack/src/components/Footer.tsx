efb140f219d611e74a83cbceb17511d18dd7a9a2
arafat-rahaman-28 <himelrizvi970@gmail.com>
2026-09-21 11:21:58 -0800

    footer section is now mobile responsive

--- dev-stack/src/components/Footer.tsx
+++ dev-stack/src/components/Footer.tsx
@@ -1,21 +1,25 @@
 import logo from "../assets/logo-text.png";
 const Footer = () => {
   return (
-    <footer className="mt-20 py-16 bg-white border-t border-gray-100 pt-12 pb-8">
+    <footer className="mt-20 py-16 bg-white border-t border-gray-100 pt-12 pb-8 md:container md:mx-auto mt-5 max-w-85 mx-auto">
       <div className="top-section flex justify-between container mx-auto">
-        <div className="left-content">
-          <img className="h-[22px] mb-3" src={logo} alt="" />
-          <p className="mb-3 text-[#64748B] w-[500px]">
+        <div className="left-content max-w-[320px] mx-auto text-[12px] text-center md:text-left">
+          <img
+            className="md:h-[22px] h-[32px] mb-3 mx-auto"
+            src={logo}
+            alt=""
+          />
+          <p className="mb-3 text-[#64748B] md:w-[500px] text-center md:text-left">
             Curated tools, technologies, and resources for developers building
             modern software.
           </p>
-          <div className="flex gap-3 text-[#475569] font-semibold text-[12px]">
+          <div className="flex gap-3 justify-center text-[#475569] font-semibold text-[12px] text-center">
             <p>GitHub</p>
             <p>Twitter</p>
             <p>LinkedIn</p>
           </div>
         </div>
-        <div className="right-content flex justify-between gap-30">
+        <div className="right-content hidden md:flex justify-between gap-30">
           <div className="product">
             <h1 className="font-semibold text-[16px] mb-4">PRODUCT</h1>
             <p className="font-semibold text-[12px] mb-4 text-[#64748B]">
@@ -52,7 +56,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <hr className="container mx-auto bg-white border-t border-gray-100 mb-8" />
+      <hr className="container mx-auto bg-white border-t border-gray-200 mt-3 mb-8" />
 
       <div className="bottom-section container mx-auto flex justify-between">
         <div className="copy-right-msg text-[#94A3B8] text-[12px]">
